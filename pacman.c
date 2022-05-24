ba1ea9c0e272f3228d4b83a0c02d0e99b86ec45d
clairedelachaux <62568093+clairedelachaux@users.noreply.github.com>
2022-05-24 12:37:19 +0200

    ajout de la fonction next_step

--- pacman.c
+++ pacman.c
@@ -11,7 +11,7 @@
 void initializeQ()
 {
     Q = malloc(NB_STATES * sizeof(float*)); //nombre d'etat = nb colonnes * nb lignes
-     for(int i=0; i<rows*cols; i++) {
+     for(int i=0; i<NB_STATES; i++) {
          Q[i] = malloc(NB_ACTIONS * sizeof(float));
          for(int j=0; j< NB_ACTIONS; j++){
              Q[i][j]=0;
@@ -19,14 +19,19 @@ void initializeQ()
      }
 }
 
+void initialize_state(){
+    int* state = malloc(10*sizeof(int));
+    new_state(current_row, current_col, state);
+    s = calc_state(state);
+}
+
 void afficher_Q(){
     for(int i=0; i<NB_STATES; i++){
         for (int j=0; j<NB_ACTIONS; j++){
             printf("%f ", Q[i][j]);
         }
-        printf("\n");
     }
-
+    printf("\n");
 }
 
 int eps_greedy(int s){
@@ -34,7 +39,6 @@ int eps_greedy(int s){
     float nb_alea = (1.*rand())/RAND_MAX;
     if (nb_alea<eps){
         a = rand()%4;
-        //printf(" greedy ");
     }
     else{;
         for (int a_i=1; a_i< NB_ACTIONS; a_i++){
@@ -51,82 +55,110 @@ int eps_greedy(int s){
     return a;
 }
 
-void calculate_new_state(int a){
+int calculate_new_state(int a, int* n_state){
     int i = current_row;
     int j = current_col;
+    int out;
+    printf("%d", a);
     switch (a)
     {
-        case east:
-            if ((j=!1) && (maze[i][j-1] == ' ')){
-                new_state(i, j-1);
-                
-            }
-            break;
+    case west:
+        if ((j=!1) && (maze[i][j-1] == ' ')){
+            out = new_state(i, j-1, n_state);
+        }else{out = new_state(i, j, n_state);}
+    break;
         
-        case north:
-            if ((i=!1) && (maze[i-1][j] == ' ')){
-                new_state(i-1, j);
-            }
+    case north:
+        if ((i=!1) && (maze[i-1][j] == ' ')){
+            out = new_state(i-1, j, n_state);
+            }else{out = new_state(i, j, n_state);}
             break;
 
-        case west:
+        case east:
             if ((j=! (cols-2)) && (maze[i][j+1] == ' ')){
-                new_state(i, j+1);
+                out = new_state(i, j+1, n_state);
             }
+            else{out = new_state(i, j, n_state);}
             break;
 
         case south:
             if ((i=!(rows-2)) && (maze[i+1][j] == ' ')){
-                new_state(i+1, j);
-            }
+                out = new_state(i+1, j, n_state);
+            }else{out = new_state(i, j, n_state);}
             break;
         default:
             break;
     }
+    return out;
 }
 
-void new_state(int i, int j){
+int new_state(int i, int j, int* n_state){
     if(maze[i-1][j]=='+'){
-        state[0] = true;
+        n_state[0] = true;
+    }else{
+        n_state[0] = false;
     }
     if(maze[i][j-1]=='+'){
-        state[1] = true;
+        n_state[1] = true;
+    }else{
+        n_state[1] = false;
     }
     if(maze[i+1][j]=='+'){
-        state[2] = true;
+        n_state[2] = true;
+    }else{
+        n_state[2] = false;
     }
     if(maze[i][j+1]=='+'){
-        state[3] = true;
+        n_state[3] = true;
+    }else{
+        n_state[3] = false;
     }
-    state[4] = nearest_target(i, j);
+    n_state[4] = nearest_target(i, j);
     int k=0;
     while((i+k<rows)&&(maze[i+k][j]!='g')){
         k++;
     }
-    if(i+k==rows){
-        state[5+south] = false;
+    if(i+k>=rows){
+        n_state[5+south] = false;
+    }else{
+        n_state[5+south] = true;
     }
     k=0;
     while((i-k>=0)&&(maze[i-k][j]!='g')){
-        k--;
+        k++;
     }
     if(i-k==-1){
-        state[5+north] = false;
+        n_state[5+north] = false;
+    }else{
+        n_state[5+north] = true;
     }
     k=0;
     while((j+k<cols)&&(maze[i][j+k]!='g')){
         k++;
     }
-    if(i+k==rows){
-        state[5+east] = false;
+    if(j+k==cols){
+        n_state[5+west] = false;
+    }else{
+        n_state[5+west] = true;
     }
     k=0;
     while((i-k>=0)&&(maze[i][j-k]!='g')){
-        k--;
+        k++;
     }
-    if(i+k==-1){
-        state[5+west] = false;
+    if(i-k==-1){
+        n_state[5+east] = false;
+    }else{
+        n_state[5+east] = true;
     }
+    n_state[10] = is_trapped(state);
+    for(int k=0; k<10; k++){
+        printf("%d- ", n_state[k]);
+    }
+    return calc_state(n_state);
+}
+
+int calc_state(int* s){
+    return 2*(s[0]+ 2*(s[1]+ 2*(s[2]+ 2*(s[3]+ 4*(s[4]+ 2*(s[5]+ 2*(s[6]+ 2*(s[8]+ 2*(s[9]+ 2*s[7])))))))));
 }
 
 void min(int*t, int n, int* val_min, int* ind_min){
@@ -140,6 +172,19 @@ void min(int*t, int n, int* val_min, int* ind_min){
             *ind_min = k;
         }
     }
+}
+
+float max_a(int s_prime, int a){
+    float M = Q[s_prime][0];
+    for (int k=1; k<4; k++){
+        if(Q[s_prime][k]>M){
+            M = Q[s_prime][k];
+        }
+        if((Q[s_prime][k]==M)&&(rand()%2==0)){
+            M = Q[s_prime][k];
+        }
+    } 
+    return M;
 }
 
 int nearest_target(int i, int j){
@@ -161,13 +206,13 @@ int nearest_target(int i, int j){
     while(maze[i-k][j]==' '){
         k++;
     }
-    if (maze[i+k][j]=='+'){
+    if (maze[i-k][j]=='+'){
         nearest_pill[north] = 50;
     }
-    if(maze[i+k][j]=='.'){
+    if(maze[i-k][j]=='.'){
         nearest_pill[north] = k;
     }
-    if(maze[i+k][j]=='g'){
+    if(maze[i-k][j]=='g'){
         nearest_pill[north] = 100;
     }
     k=0;
@@ -201,18 +246,104 @@ int nearest_target(int i, int j){
     return ind_min;
 }
 
-int is_trapped(int* state){
-    if(!state[north]&&(!state[north+5])){
+int is_trapped(int* s){
+    if(!s[north]&&(!s[north+5])){
         return false;
     }
-    if(!state[south]&&(!state[south+5])){
+    if(!s[south]&&(!s[south+5])){
         return false;
     }
-    if(!state[east]&&(!state[east+5])){
+    if(!s[east]&&(!s[east+5])){
         return false;
     }
-    if(!state[west]&&(!state[west+5])){
+    if(!s[west]&&(!s[west+5])){
         return false;
     }
     return true;
+}
+
+int next_step(){
+    int a = eps_greedy(s);
+    switch (a)
+    {
+    case north:
+        next_row = current_row-1;
+        next_col = current_col;
+        break;
+    
+    case south:
+        next_row = current_row+1;
+        next_col = current_col;
+        break;
+
+    case west:
+        next_col = current_col-1;
+        next_row = current_row;
+        break;
+    
+    case east:
+        next_col = current_col+1;
+        next_row = current_row;
+        break;
+    
+    default:
+        break;
+    }
+    int reward = 0;
+    switch (maze[next_row][next_col])
+    {
+    case 'g':
+        reward = -100;
+        nb_life--;
+        for (int g=0; g<3; g++){
+            if (gosts_last_case[g]=='p'){
+                maze[gosts_pos[g][0]][gosts_pos[g][1]] = ' ';
+            }else{
+                maze[gosts_pos[g][0]][gosts_pos[g][1]] = gosts_last_case[g];
+            }
+            gosts_last_case[g] = ' ';
+            gosts_pos[g][0] = gosts_start_pos[g][0];
+            gosts_pos[g][1] = gosts_start_pos[g][1];
+        }
+        break;
+
+    case '+':
+        reward = -10;
+        next_col = current_col;
+        next_row = current_row;
+    break;
+
+    case '.':
+        reward = 10;
+        nb_food--;
+    break;
+    
+    default:
+        break;
+
+    }
+    if (nb_food==0){
+        return 1;
+    }
+    if (nb_life==0){
+        return -1;
+    }
+    maze[current_row][current_col] = ' ';
+    int s_prime = calculate_new_state(a, state);
+    printf("%d%d", s, s_prime);
+    Q[s][a] += alpha*(reward + gamma*(max_a(s_prime, a)-Q[s][a]));
+    current_col = next_col;
+    current_row = next_row;
+    maze[current_row][current_col]= 'p';
+    move_gosts();
+    s = s_prime;
+    printf("\n");
+    return 0;
+}
+
+void free_Q(){
+    for(int i=0; i<NB_STATES; i++){
+        free(Q[i]);
+    }
+    free(Q);
 }
