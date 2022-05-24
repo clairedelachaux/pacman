3be9222741c17df43cd07ebbcc4eeffe669ade03
clairedelachaux <62568093+clairedelachaux@users.noreply.github.com>
2022-05-24 12:38:22 +0200

    ajout de la fonction next_step

--- pacman.h
+++ pacman.h
@@ -3,22 +3,35 @@
 #include "env.h"
 
 int state[10];
-int state_curr;
+int s;
+int nb_life;
+int nb_food;
+float** Q;
 
 void initializeQ();
+
+void initialize_state();
 
 int eps_greedy(int s);
 
 void afficher_Q();
 
-void calculate_new_state(int a);
+int calculate_new_state(int a, int* n_state);
 
 void min(int*t, int n, int* val_min, int* ind_min);
 
-void new_state(int i, int j);
+float max_a(int s_prime, int a);
+
+int calc_state(int* s);
+
+int new_state(int i, int j, int* n_state);
 
 int nearest_target(int i, int j);
 
-float** Q;
+int is_trapped(int* s);
+
+int next_step();
+
+void free_Q();
 
 #endif
