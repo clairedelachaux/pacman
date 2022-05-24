b9c88c38b4ade96c6e2d6edfef472ece970d4568
clairedelachaux <62568093+clairedelachaux@users.noreply.github.com>
2022-05-24 12:36:04 +0200

    ajout de la fonction maze_reset

--- env.h
+++ env.h
@@ -3,15 +3,18 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include<string.h>
 
 char** maze;
+char** maze_init;
 int rows;
 int cols;
 int start_row;
 int start_col;
 int current_row;
 int current_col;
+int next_row;
+int next_col;
 int gosts_pos[3][2];
 char gosts_last_case[3];
 int gosts_start_pos[3][2];
@@ -27,8 +30,9 @@ enum direction{
 
 void alloc_maze();
 void make_maze(char* file_name);
+void maze_reset();
 void move_gosts();
-void move_one_gost(int g, int dplct);
+int move_one_gost(int g, int dplct);
 void maze_render();
 
 
